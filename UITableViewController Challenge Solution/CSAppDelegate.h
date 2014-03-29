9f48d3676dae5c241f75a97f12beff2f1131503f
Javier González Rojo <Javier@MacBook-Air-de-Javier.local>
2014-03-29 13:14:14 +0100

    Initial Commit

---
+++ UITableViewController Challenge Solution/CSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSAppDelegate.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Javier González Rojo on 29/03/14.
+//  Copyright (c) 2014 Javier González Rojo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UITableViewController Challenge Solution/CSViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSViewController.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Javier González Rojo on 29/03/14.
+//  Copyright (c) 2014 Javier González Rojo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSViewController : UIViewController
+
+@end
